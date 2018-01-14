be4c33d8e461fcfd426a8332fb8be7936e24dc9c
komlanvi <komlanvi.amegbleame@gmail.com>
2018-01-14 22:50:25 +0100

    Some utils queries to get categories

--- lib/rumbl/app/category.ex
+++ lib/rumbl/app/category.ex
@@ -15,4 +15,30 @@ defmodule Rumbl.App.Category do
     |> cast(attrs, [:name])
     |> validate_required([:name])
   end
+
+  @doc """
+  Returns a query to list categories ordered by name.
+
+  ## Examples
+
+      iex> Repo.all categories_ordered_by_name()
+      [%Category{name: "A"}, %Category{name: "B"}, ...]
+
+  """
+  def categories_ordered_by_name(query) do
+    from c in query, order_by: c.name
+  end
+
+  @doc """
+  Returns a query that list categories as {name, id}.
+
+  ## Examples
+
+      iex> Repo.all categories_ordered_by_name()
+      [{name: "A", id: 1}, {name: "C", id: 2}, ...]
+
+  """
+  def categories_name_and_id(query) do
+    from c in query, select: {c.name, c.id}
+  end
 end
